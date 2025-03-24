c1033954416c260652d638992e7c91aab70185cd
Muhammad Abdullah Yusuf <abdullahyusuf.bmiib@gmail.com>
2025-03-24 13:10:09 +0300

    feat: add inventory update functionality, add confirmation dialog for deletion to the repo (missed in the last commit)

---
+++ prac-gadget-shop/src/app/components/dialog-box/dialog-box.component.ts
@@ -0,0 +1,17 @@
+import {Component, inject} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-dialog-box',
+  imports: [],
+  templateUrl: './dialog-box.component.html',
+  styleUrl: './dialog-box.component.css'
+})
+export class DialogBoxComponent {
+
+  modal = inject(NgbActiveModal);
+
+  confirm() {
+    this.modal.close({event: "confirm"});
+  }
+}
