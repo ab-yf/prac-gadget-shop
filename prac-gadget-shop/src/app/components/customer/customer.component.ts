1bf69433aa2e9aedab2772f4acb86170bed736ae
Muhammad Abdullah Yusuf <abdullahyusuf.bmiib@gmail.com>
2025-03-26 10:34:05 +0300

    feat: fetch customer data after adding customer, add prettier

--- prac-gadget-shop/src/app/components/customer/customer.component.ts
+++ prac-gadget-shop/src/app/components/customer/customer.component.ts
@@ -1,35 +1,39 @@
-import {Component, inject} from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {AddCustomerDialogComponent} from '../add-customer-dialog/add-customer-dialog.component';
-import {HttpClient} from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddCustomerDialogComponent } from '../add-customer-dialog/add-customer-dialog.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-customer',
-  imports: [],
-  templateUrl: './customer.component.html',
-  styleUrl: './customer.component.css'
+    selector: 'app-customer',
+    imports: [],
+    templateUrl: './customer.component.html',
+    styleUrl: './customer.component.css',
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+    httpClient = inject(HttpClient);
+    customerData: any;
+    private modalService = inject(NgbModal);
 
-  httpClient = inject(HttpClient);
-  private modalService = inject(NgbModal);
+    openAddCustomerDialog() {
+        this.modalService
+            .open(AddCustomerDialogComponent)
+            .result.then((data) => {
+                if (data.event == 'closed') {
+                    this.getCustomerData();
+                }
+            });
+    }
 
-  customerData: any;
+    ngOnInit(): void {
+        this.getCustomerData();
+    }
 
-  openAddCustomerDialog(){
-    this.modalService.open(AddCustomerDialogComponent)
-  }
-
-  ngOnInit(): void {
-    this.getCustomerData();
-  }
-
-  getCustomerData() {
-    // Get customer data from database
-    const apiUrl = "https://localhost:7107/api/Customer";
-    this.httpClient.get(apiUrl).subscribe( result => {
-      this.customerData = result;
-      console.log(this.customerData);
-    })
-  }
+    getCustomerData() {
+        // Get customer data from database
+        const apiUrl = 'https://localhost:7107/api/Customer';
+        this.httpClient.get(apiUrl).subscribe((result) => {
+            this.customerData = result;
+            console.log(this.customerData);
+        });
+    }
 }
